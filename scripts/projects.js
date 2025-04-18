4c6cb42a973b38a8b12560769d050bec5d6683a7
Johannes Roth <jrothx@icloud.com>
2025-04-18 16:14:32 +0200

    works on projects.html

--- scripts/projects.js
+++ scripts/projects.js
@@ -2,10 +2,10 @@ const apps = [
     {
         title: 'Join',
         imagePath: 'img/join.png',
-        stack: [],
+        stack: ['icons/html.svg', 'icons/css.svg', 'icons/javascript.svg', 'icons/firebase.svg',],
         description: '',
         link: '',
-        show:true,
+        show: true,
     },
     {
         title: 'DABubble',
@@ -13,7 +13,7 @@ const apps = [
         stack: [],
         description: '',
         link: '',
-        show:true,
+        show: true,
     },
 ];
 
@@ -24,7 +24,7 @@ const websites = [
         stack: [],
         description: '',
         link: '',
-        show:true,
+        show: true,
     },
     {
         title: 'Natalie Schlindwein',
@@ -32,7 +32,7 @@ const websites = [
         stack: [],
         description: '',
         link: '',
-        show:true,
+        show: true,
     },
     {
         title: 'Johannes Roth',
@@ -40,9 +40,14 @@ const websites = [
         stack: [],
         description: '',
         link: '',
-        show:true,
+        show: true,
     },
 ];
+
+const projects = {
+    apps: apps,
+    websites: websites,
+}
 
 function renderprojects() {
     renderApps('apps');
@@ -50,10 +55,7 @@ function renderprojects() {
 }
 
 function renderApps(divID) {
-    const projects = {
-        apps: apps,
-        websites: websites,
-    }
+
     const projectArray = projects[divID];
     console.log(projectArray);
 
@@ -62,13 +64,91 @@ function renderApps(divID) {
     div.innerHTML = '';
     for (let index = 0; index < projectArray.length; index++) {
         const project = projectArray[index];
-        if (project.show===true) {
-            div.innerHTML += `<div class="projectCard">
+        if (project.show === true) {
+            div.innerHTML += `<div onclick="openOverlay(${index}, '${divID}')" class="projectCard">
             <img src="${project.imagePath}" alt="">
            
                             </div>`
         }
-      
+
     }
 
+}
+
+function openOverlay(index, divID) {
+    console.log(index);
+    console.log(divID);
+    const overlay = document.getElementById('projectsOverlay');
+    overlay.classList.remove('dnone');
+    renderOverlayView(index, divID, overlay);
+
+
+}
+
+
+function renderOverlayView(index, divID, overlay) {
+    const projectArray = projects[divID];
+    console.log(projectArray);
+    overlay.innerHTML = '';
+    const project = projectArray[index];
+    console.log(project);
+    overlay.innerHTML += `<button class="closeButton">X</button>
+                        <div class="viewContainer">
+                        <div class="viewContent">
+                        <img class="projectImage" src="${project.imagePath}" alt="">
+                        
+                           <div class="projectDetails">
+                             <span>${project.title}</span>
+                            <span>${project.description}</span>
+                             <div class="stack">${renderStack(index, divID)}</div>
+                         </div>
+                        
+                        
+                        
+                        </div>
+       
+    
+                         <div class="btnContainer">
+                        <button onclick="step(${index}, '${divID}','back', event)"><</button>
+                        <button onclick="step(${index}, '${divID}','forward', event)">></button>
+                        </div>
+                        </div>
+                        
+                       `
+
+}
+
+function renderStack(index, divID) {
+    const projectArray = projects[divID];
+    const project = projectArray[index];
+    const stack = project.stack
+
+    let stackHTML = '';
+    for (let stackIndex = 0; stackIndex < stack.length; stackIndex++) {
+        const stackImage = stack[stackIndex];
+        console.log(stackImage);
+        stackHTML += `<img class="stacks" src="${stackImage}" alt="">`
+    }
+    return stackHTML
+}
+
+function step(index, divID, step, event) {
+    let stepIndex = step === 'back' ? index - 1 : index + 1;
+    const projectArray = projects[divID];
+    console.log(stepIndex);
+    if (stepIndex < 0) {
+        stepIndex = projectArray.length - 1
+    }
+    if (stepIndex >= projectArray.length) {
+        stepIndex = 0;
+    }
+    const overlay = document.getElementById('projectsOverlay');
+    renderOverlayView(stepIndex, divID, overlay);
+    event.stopPropagation();
+}
+
+
+function closeOverlay() {
+    const overlay = document.getElementById('projectsOverlay');
+    overlay.classList.add('dnone');
 }
